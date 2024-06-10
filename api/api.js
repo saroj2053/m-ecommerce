b6fa9793757248a78f548b0fa80aa30833ce729b
Saroj Sah <sarojsaroj390@gmail.com>
2024-06-10 12:20:23 +0200

    major ui improvements

--- api/api.js
+++ api/api.js
@@ -1,32 +1,32 @@
-var router = require('express').Router();
-var async = require('async');
+var router = require("express").Router();
+var async = require("async");
 
-var Category = require('../models/category');
-var Product = require('../models/product');
+var Category = require("../models/category");
+var Product = require("../models/product");
 
-router.get('/:name', function(req, res, next) {
-	async.waterfall([
-		function(callback) {
-			Category.findOne({ name: req.params.name }, function(err, category) {
-				if (err) return next(err);
-				callback(null, category);
-			});
-		},
-		function(category, callback) {
-		
-			var product = new Product();
-			product.category = category._id;
-			product.name = req.body.productName;
-			product.color = req.body.productColor;
-			product.productMaterial = req.body.productMaterial;
-			product.price = req.body.productPrice;
-			product.image = req.body.productImage;
-			product.description = req.body.productDesc;
+router.post("/:name", function (req, res, next) {
+  console.log("hello, world");
+  async.waterfall([
+    function (callback) {
+      Category.findOne({ name: req.params.name }, function (err, category) {
+        if (err) return next(err);
+        callback(null, category);
+      });
+    },
+    function (category, callback) {
+      var product = new Product();
+      product.category = category._id;
+      product.name = req.body.productName;
+      product.color = req.body.productColor;
+      product.productMaterial = req.body.productMaterial;
+      product.price = req.body.productPrice;
+      product.image = req.body.productImage;
+      product.description = req.body.productDesc;
 
-			product.save();
-		}
-	]);
-	res.json({ message: 'success' });
+      product.save();
+    },
+  ]);
+  res.json({ message: "success" });
 });
 
 module.exports = router;
